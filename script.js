5525af00938bf4a1b985275c7f2c868d3368d574
Luu Van <vanngocluu94@gmail.com>
2021-03-07 23:34:37 +0700

    caculator

--- script.js
+++ script.js
@@ -8,5 +8,27 @@ const newYears = "1 Jan 2022";
 function countDown() {
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    //console.log(newYearsDate - currentDate);
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
 }
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+countDown();
+
+setInterval(countDown, 1000);
 
