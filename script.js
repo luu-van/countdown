3205424103addcc15f426353c6c7d920b4eca5a0
Luu Van <vanngocluu94@gmail.com>
2021-03-07 23:23:53 +0700

    create function countDown

---
+++ script.js
@@ -0,0 +1,12 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+const newYears = "1 Jan 2022";
+
+function countDown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+}
+
